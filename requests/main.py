ea43d1f8186c072939589b1382fa91ff72d9c2b1
Juan Pablo Espinoza <juanespinoza830@pm.me>
2023-10-30 01:17:29 -0300

    add some comments, remove unnecesary requests

--- requests/main.py
+++ requests/main.py
@@ -3,7 +3,6 @@
 
 import requests
 import json
-import uuid
 
 # create session
 headers = { 
@@ -12,7 +11,7 @@ headers = {
 session = requests.Session()
 session.headers.update(headers)
 
-### get csrf token
+### get csrf token from the form in the login page
 url = "https://informesdeis.minsal.cl/SASVisualAnalytics/"
 response = session.get(url)
 csrf_token = session.cookies.get_dict()["X-Uaa-Csrf"]
@@ -27,25 +26,20 @@ payload = {
 }
 response = session.post(url, data=payload)
 
-#### get an executor
+#### get an executor, the executor is used to get data from the dashboard
+# the website seems to get the executor through a complex process with some authorizes ending on a post
+# I couldn't imitate it, but the get request gets it directly with no nuance, took me a while to figure it out
 url = 'https://informesdeis.minsal.cl/reportData/executors'
-response = session.get(url)
+response = session.get(url) 
 
 # extract the executorid
 response = json.loads(response.text)
 executorid = response["items"][0]["id"]
 
-### get the data definitions from the dashboard
-url = 'https://informesdeis.minsal.cl/reports/reports/ad0c03ad-ee7a-4da4-bcc7-73d6e12920cf/content'
-header = {
-    "Accept": "application/vnd.sas.report.content+json" # force json version instead of xml one
-}
-response = session.get(url, headers=header)
-
-print(response.text)
-
 ### try to get the data by force using the acquired executorid
-url = f'https://informesdeis.minsal.cl/reportData/jobs?indexStrings=true&embeddedData=true&executorId={executorid}&wait=30&jobId={executorid}_c1&sequence=2&dataDefinitions=dd34112'
+# no idea where the data definition comes from, but it looks like thats all we need
+# if you need to scrape from another dashboard, you can get the data definition from the network tab in the browser
+url = f'https://informesdeis.minsal.cl/reportData/jobs?indexStrings=true&embeddedData=true&executorId={executorid}&wait=30&jobId={executorid}_c0&sequence=1&dataDefinitions=dd34112'
 response = session.get(url)
 
 print(response.text)
