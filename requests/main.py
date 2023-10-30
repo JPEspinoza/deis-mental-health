4300dec90edb56e31df0fd0d3fb50144a46b54af
Juan Pablo Espinoza <juanespinoza830@pm.me>
2023-10-30 01:05:29 -0300

    getting executor, getting dashboard, no reportData (site broken?)

--- requests/main.py
+++ requests/main.py
@@ -28,18 +28,6 @@ payload = {
 response = session.post(url, data=payload)
 
 #### get an executor
-# this is an opaque ritual
-# I don't understand where the state comes from, i think the frontend generates a random one, so just putting a random string works
-
-url = 'https://informesdeis.minsal.cl/reportData/executors'
-session.post(url)
-
-url = 'https://informesdeis.minsal.cl/SASLogon/oauth/authorize?client_id=sas.reportData&redirect_uri=/reportData/executors?sso_retry=POST&response_type=code&state=7bb636be'
-session.get(url)
-
-url = 'https://informesdeis.minsal.cl/reportData/executors?sso_retry=POST&code=tV6RLE79Dd&state=7bb636be'
-session.get(url)
-
 url = 'https://informesdeis.minsal.cl/reportData/executors'
 response = session.get(url)
 
@@ -47,21 +35,17 @@ response = session.get(url)
 response = json.loads(response.text)
 executorid = response["items"][0]["id"]
 
-### try to get the data by force using the acquired executorid
-url = f'https://informesdeis.minsal.cl/reportData/jobs?indexStrings=true&embeddedData=true&executorId={executorid}&wait=30&jobId={executorid}_c0&sequence=1'
-response = session.post(url)
-
-print(executorid)
-print(response.text)
-
-
-exit()
-
-### get the dashboard first page 
+### get the data definitions from the dashboard
 url = 'https://informesdeis.minsal.cl/reports/reports/ad0c03ad-ee7a-4da4-bcc7-73d6e12920cf/content'
 header = {
     "Accept": "application/vnd.sas.report.content+json" # force json version instead of xml one
 }
 response = session.get(url, headers=header)
 
-### extract data definitions from response
+print(response.text)
+
+### try to get the data by force using the acquired executorid
+url = f'https://informesdeis.minsal.cl/reportData/jobs?indexStrings=true&embeddedData=true&executorId={executorid}&wait=30&jobId={executorid}_c1&sequence=2&dataDefinitions=dd34112'
+response = session.get(url)
+
+print(response.text)
