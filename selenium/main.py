f25eda198781349e1d0023430954ef29b9d06622
Juan Pablo Espinoza <juanespinoza830@pm.me>
2023-11-02 19:57:34 -0300

    selenium looping on lists

--- selenium/main.py
+++ selenium/main.py
@@ -10,34 +10,75 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
+import itertools
+
+def get_list_items(button_id, list_id):
+    sleep(2)
+
+    # open dropdown
+    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.ID, button_id))).click()
+
+    # get list items
+    list = driver.find_element(By.ID, list_id)
+    items = list.find_elements(By.TAG_NAME, "li")
+
+    # remove the first item as it is the "All" option
+    items.pop(0)
+    items = [item.text for item in items]
+    
+    # close dropdown
+    WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.ID, button_id))).click()
+
+    sleep(2)
+
+
+    return items 
 
 driver = webdriver.Chrome()
   
 driver.get("https://informesdeis.minsal.cl/SASVisualAnalytics/?reportUri=%2Freports%2Freports%2F0b3119f0-db06-4f10-a9cd-61092b5790bc&sectionIndex=0&sso_guest=true&reportViewOnly=true&reportContextBar=false&sas-welcome=false")
-driver.set_window_size(1045, 671)
+driver.set_window_size(1280, 800)
 
-WebDriverWait(driver, 30).until(expected_conditions.frame_to_be_available_and_switch_to_it((By.ID,"VANextLogon_iframe")))
+# wait for the page to load
+WebDriverWait(driver, 60).until(expected_conditions.frame_to_be_available_and_switch_to_it((By.ID,"VANextLogon_iframe")))
 
-# we might crash the frontend as it takes too long to close the popup so we wait for two seconds
+# hide error message
 WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.ID,"__dialog1-closeButton-BDI-content"))).click()
-sleep(2)
 
 # find the parent of the element with the text "Salud mental" and click it
 WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.XPATH,"//*[contains(text(), 'Salud mental')]/.."))).click()
 
-# find the dropdowns with the region, servicio, comuna and establecimiento
+### find the dropdowns with the region, servicio, comuna and establecimiento
 # get all the values for each of the 4 dropdowns
 # for each value, select it and wait for the table to load
 # then, click the export button and wait for the download to finish
+# first dropdown (regiones) __select0 and get values from __list2
+# second dropdown (servicios) __select1 and get values from __list3
+# third dropdown (comunas) __select2 and get values from __list4
+# fourth dropdown (establecimientos) __select3 and get values from __list5
 
-# open first drop-down
-# get all options
-WebDriverWait(driver, 30).until(expected_conditions.presence_of_element_located((By.ID,"__select0"))).click()
+for region in get_list_items("__select0", "__list2"):
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "__select0"))).click()
+    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, f"[title*=\"Región: {region}\"]"))).click()
+    for servicio in get_list_items("__select1", "__list3"):
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "__select1"))).click()
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, f"[title*=\"Servicio de Salud: {servicio}\"]"))).click()
+        for comuna in get_list_items("__select2", "__list4"):
+            comuna = comuna.replace("'", "\\'") # replace O'HIGGINS with O\'HIGGINS
+            print(comuna)
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "__select2"))).click()
+            WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, f"[title*=\"Comuna: {comuna}\"]"))).click()
+            for establecimiento in get_list_items("__select3", "__list5"):
+                WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.ID, "__select3"))).click()
+                WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, f"[title*=\"Establecimiento: {establecimiento}\"]"))).click()
+                sleep(2)
+
+                # click the more icon
+
+                # click export
+
+                # download the report
+
+                # rename file
+
 sleep(100)
-regiones_list = driver.find_element(By.ID,"__list29")
-print(regiones_list)
-regiones = regiones_list.find_elements(By.TAG_NAME, "li")
-print(regiones)
-
-sleep(100)
-
