9eefe6988903226d5dbb4a6ac7c63a6e0d1d4fbd
Juan Pablo Espinoza <juanespinoza830@pm.me>
2023-11-03 01:18:07 -0300

    working selenium approach

--- requests/main.py
+++ requests/main.py
@@ -11,11 +11,21 @@ headers = {
 session = requests.Session()
 session.headers.update(headers)
 
-report_id = "0b3119f0-db06-4f10-a9cd-61092b5790bc"
-
 # enter site first to get cookies
-session.get(f"https://informesdeis.minsal.cl/SASVisualAnalytics/?reportUri=%2Freports%2Freports%2F{report_id}&sectionIndex=0&sso_guest=true&reportViewOnly=true&reportContextBar=false&sas-welcome=false")
+session.get(f"https://informesdeis.minsal.cl/SASVisualAnalytics/?reportUri=%2Freports%2Freports%2F0b3119f0-db06-4f10-a9cd-61092b5790bc&sectionIndex=0&sso_guest=true&reportViewOnly=true&reportContextBar=false&sas-welcome=false")
 
-# download content
-response = session.get(f"https://informesdeis.minsal.cl/reports/reports/{report_id}/content")
+
+
+# executor
+response = session.get("https://informesdeis.minsal.cl/reportData/executors")
+
+executor = json.loads(response.text)["items"][0]["id"]
+print(executor)
+
+
+
+
+
+# content
+response = session.post(f"https://informesdeis.minsal.cl/reportData/jobs?indexStrings=false&embeddedData=false&executorId={executor}&wait=30&sequence=39&dataDefinitions=dd14540")
 print(response.text)
